cc311ef30b5cab6032b50bc49588843d99b5e99c
Saif Ullah <saif.ullah.3696@gmail.com>
2021-04-07 16:01:46 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import useFetchPokemon from "../useRequest";
+import Pokemon from "../components/Pokemon";
+
+export default function IndexPage() {
+  const { result, error } = useFetchPokemon();
+
+  if (error) return <h1>Something went wrong!</h1>;
+  if (!result) return <h1>Loading...</h1>;
+
+  return (
+    <main className="App">
+      <h1>My pokemons</h1>
+      <div>
+        {result.results.map((pokemon) => (
+          <Pokemon pokemon={pokemon} key={pokemon.name} />
+        ))}
+      </div>
+    </main>
+  );
+}
